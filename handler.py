c54725648d49199078fd0fdedd8a4b3a192915aa
dmitry <karpesh.dmitry@gmail.com>
2017-11-15 19:55:15 +0300

    init s3 trigger

--- handler.py
+++ handler.py
@@ -8,11 +8,11 @@ import logging
 print('Loading function')
 
 
-def put_item(item):
-    table_name = 'test'
-    db = boto3.resource('dynamodb', region_name='us-east-2')
-    table = db.Table(table_name)
-    table.put_item(Item=item)
+# def put_item(item):
+#     table_name = 'test'
+#     db = boto3.resource('dynamodb', region_name='us-east-2')
+#     table = db.Table(table_name)
+#     table.put_item(Item=item)
 
 
 def lambda_handler(event, context):
@@ -20,22 +20,25 @@ def lambda_handler(event, context):
     logger.setLevel(logging.INFO)
     res = ''
     try:
+        table_name = 'test'
+        db = boto3.resource('dynamodb', region_name='us-east-2')
+        table = db.Table(table_name)
+
         s3 = boto3.resource('s3')
-        bucket = s3.Bucket('input-bucket-csv')
+        bucket = s3.Bucket('input-csv')
         content = bucket.Object('shopee_order.csv').get()['Body'].read().splitlines()
         reader = csv.DictReader(content)
         for row in reader:
             data = {k: v for k, v in row.items() if v}
-            put_item(data)
-            res += str(data)
+            table.put_item(Item=data)
+            # res += str(data)
 
     except Exception as e:
         print(e)
-        res = str(e)
         raise e
 
-    response = {
-        "statusCode": 200,
-        "body": res
-    }
-    return response
+    # response = {
+    #     "statusCode": 200,
+    #     "body": res
+    # }
+    return None
